691cff883407e11987b81e91ee2d267ff483095e
miaozi <1026937338@qq.com>
2022-05-06 23:46:01 +0800

    init

---
+++ Netty_Starter/src/main/java/com/nettystar/netty/dubborpc/inter/HelloService.java
@@ -0,0 +1,5 @@
+package com.nettystar.netty.dubborpc.inter;
+
+public interface HelloService {
+    String hello(String msg);
+}
---
+++ Netty_Starter/src/main/java/com/nettystar/netty/dubborpc/provider/ServerStart.java
@@ -0,0 +1,8 @@
+package com.nettystar.netty.dubborpc.provider;
+
+public class ServerStart {
+
+    public static void main(String[] args) {
+        // #TODO
+    }
+}
